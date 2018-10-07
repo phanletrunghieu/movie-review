91c5a01f2824721199d5b45e67df57ec45eaeb37
Phan Hieu <hieutrunglephan@gmail.com>
2018-10-08 01:20:40 +0700

    login screen

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -1,5 +1,15 @@
+import { AsyncStorage } from 'react-native'
+
 export function CheckToken(token) {
     return new Promise((resolve, reject)=>{
         resolve()
+    })
+}
+
+export function Login(username, password) {
+    return new Promise(async (resolve, reject)=>{
+        let token = "abc"
+        await AsyncStorage.setItem('userToken', token);
+        resolve(token)
     })
 }
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,21 +1,103 @@
 import React, { Component } from 'react'
-import { Text, View, Button, AsyncStorage } from 'react-native'
+import { StyleSheet, StatusBar, View, Image, Alert } from 'react-native'
+import { Form, Item, Input, Button, Text, Icon } from 'native-base';
+import LinearGradient from 'react-native-linear-gradient';
+import {Login} from '../api/UserAPI'
 
 export default class SignInScreen extends Component {
     static navigationOptions = {
         title: 'Please sign in',
     };
 
+    state = {
+        username: "",
+        password: "",
+        showPassword: false,
+    }
+
     _signInAsync = async () => {
-        await AsyncStorage.setItem('userToken', 'abc');
-        this.props.navigation.navigate('App');
+        let {username, password} = this.state
+        Login(username, password)
+        .then(token=>{
+            this.props.navigation.navigate('App');
+        })
+        .catch(err=>Alert.alert('Error', err))
+        
     };
 
     render() {
         return (
-        <View>
-            <Button title="Sign in!" onPress={this._signInAsync} />
-        </View>
+            <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 1}} colors={['#F99F00', '#DB3069']} style={styles.linearGradient}>
+                <StatusBar backgroundColor="transparent" translucent={true} />
+                <Image source={require("../assert/logo.png")} />
+                <View style={styles.content}>
+                    <Form style={styles.form}>
+                        <Item regular style={styles.item}>
+                            <Input
+                                placeholder="Username"
+                                value={this.state.username}
+                                onChangeText={username => this.setState({username})}
+                                style={styles.input}
+                                textContentType="username"
+                                autoFocus
+                            />
+                        </Item>
+                        <Item regular style={styles.item}>
+                            <Input
+                                placeholder="Password"
+                                style={styles.input}
+                                textContentType="password"
+                                keyboardType={this.state.showPassword ? "visible-password" : "default"}
+                                secureTextEntry={true}
+                            />
+                            <Button
+                                transparent
+                                light
+                                onPress={()=>this.setState({showPassword: !this.state.showPassword})}
+                                style={styles.btnShowPass}
+                            >
+                                <Icon name={this.state.showPassword ? 'md-eye-off' : 'md-eye'} />
+                            </Button>
+                        </Item>
+                    </Form>
+                    <View style={styles.buttonContainer}>
+                        <Button rounded light block onPress={this._signInAsync} style={styles.button}>
+                            <Text>Login</Text>
+                        </Button>
+                    </View>
+                </View>
+            </LinearGradient>
         )
     }
 }
+
+var styles = StyleSheet.create({
+    linearGradient: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    content: {
+        width: "95%",
+    },
+    form: {
+        marginVertical: 5
+    },
+    item: {
+        borderColor: "#fff",
+        marginTop: 5
+    },
+    input: {
+        color: "#fff",
+    },
+    buttonContainer: {
+        width: "50%",
+        alignSelf: "center"
+    },
+    button: {
+        marginTop: 5
+    },
+    btnShowPass: {
+        marginRight: 5
+    }
+})
